2976ef855e093f8c2b97a0c6af0cca92e48d62c5
Rohail136 <Rohail.ch136@gmail.com>
2025-09-06 10:58:43 +0500

    Updating 6/9/25

--- src/Screen/SignUp.tsx
+++ src/Screen/SignUp.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image} from 'react-native'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { NativeStackNavigatorProps, NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../Navigation/RootsStackParamList'
 import ScreenName from '../Constant/ScreenName'
@@ -9,6 +9,11 @@ import Logo2 from '../assets/Logo2.jpg';
 type Props = NativeStackScreenProps<RootStackParamList, ScreenName.SignUp>
 
 const SignUp = ({navigation}: Props) => {
+  const nameRef = useRef<TextInput>(null);
+  const fatherRef = useRef<TextInput>(null);
+  const emailRef = useRef<TextInput>(null);
+  const passwordRef = useRef<TextInput>(null);
+  const reenterRef = useRef<TextInput>(null);
   const [name, setName] = useState('');
   const [father, setFather] = useState('');
   const [email, setEmail] = useState('');
@@ -20,12 +25,27 @@ const SignUp = ({navigation}: Props) => {
   console.log('Email : ', email);
   console.log('Password : ', password);
   console.log('Re-enter : ', reenter);
-
+  // for empty input field after filling
   setName('');
   setFather('');
   setEmail('');
   setPassword('');
   setReenter('');
+  }
+  const handleFocus = () => {
+    if(!name){
+      emailRef.current?.focus();
+    } else if (!father) {
+      fatherRef.current?.focus();
+    } else if (!email) {
+      emailRef.current?.focus();
+    } else if (!password) {
+      passwordRef.current?.focus();
+    } else if (!reenter) { 
+      reenterRef.current?.focus();
+    } else {
+      handeSignUp();
+    }
   }
   return (
     
@@ -37,35 +57,50 @@ const SignUp = ({navigation}: Props) => {
              {/* <Text> {route.params?.title}</Text> */}
       <Text style={styles.SignUpText}> ----- Sign Up ----- </Text>
       <TextInput placeholder='Enter Full Name' 
+        ref={nameRef}
         style={styles.Inputfield} 
         value={name}
         onChangeText={setName}
-        autoCapitalize='words' />
+        autoCapitalize='words' 
+        onSubmitEditing={() => fatherRef.current?.focus()}
+        />
       <TextInput placeholder='Enter Father Name' 
+        ref={fatherRef}
         style={styles.Inputfield} 
         value={father}
         onChangeText={setFather} 
-        autoCapitalize='words' />
+        autoCapitalize='words' 
+        onSubmitEditing={() => emailRef.current?.focus()}
+        />
       <TextInput placeholder='Enter Email' 
+        ref={emailRef}
         style={styles.Inputfield} 
         value={email}
         onChangeText={setEmail}
         keyboardType= 'email-address'
-        autoCapitalize= 'none' />
+        autoCapitalize= 'none' 
+        onSubmitEditing={() => passwordRef.current?.focus()}
+        />
       <TextInput placeholder='Enter Password' 
+        ref={passwordRef}
         style={styles.Inputfield} 
         value={password}
         onChangeText={setPassword}
         keyboardType= 'default'
-        secureTextEntry />
+        secureTextEntry 
+        onSubmitEditing={() => reenterRef.current?.focus()}
+        />
       <TextInput placeholder='Renter Password' 
+        ref={reenterRef}
         style={styles.Inputfield} 
         value={reenter}
         onChangeText={setReenter}
         keyboardType='default'
-        secureTextEntry /> 
+        secureTextEntry 
+        onSubmitEditing={handeSignUp}
+        /> 
       <TouchableOpacity onPress={() =>{
-        handeSignUp();
+        handleFocus();
         navigation.navigate(ScreenName.HomeScreen,{
         title:'HomeScreen'})
       }}
@@ -86,33 +121,33 @@ const styles = StyleSheet.create({
   backgroundColor: '#20552765'
   },
   image : {
-  resizeMode: 'cover', 
-  width: 300, 
-  height: 160, 
-  borderRadius: 10, 
-  marginBottom: 10,
-  elevation : 5,
+    resizeMode: 'cover',
+    width: 300,
+    height: 180,
+    borderRadius: 10,
+    marginBottom: 10,
+    shadowRadius: 4,
+    elevation: 7,
   },
   SignUpButton:{
   alignItems: 'center',
   backgroundColor:'blue',
-  marginTop: 30,
-  paddingVertical : 10,
-  paddingHorizontal: 50,
+  marginTop: 20,
+  paddingVertical : 12,
+  paddingHorizontal: 55,
   borderRadius: 10,
-  marginHorizontal: 20,
   },
   SignUpText : {
    marginBottom : 20, 
    fontWeight: 'bold', 
    fontSize : 25,
-   color : '#f0f3f3be'
+   color : '#0e30ece1'
   },
   Inputfield:{
     backgroundColor: 'white',
     margin : 3,
-    width : 280,
-    height : 40,
+    width : 260,
+    height : 45,
     borderRadius: 10,
     paddingHorizontal : 10,
   }
